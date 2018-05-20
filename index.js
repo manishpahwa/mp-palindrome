9ca8c4c8386a89fc3ae4f7fd5c455aed58de84af
manishpahwa <manish_pahwa@yahoo.com>
2018-05-21 07:52:31 +1000

    starting file

---
+++ index.js
@@ -0,0 +1,52 @@
+//reverses a string
+String.prototype.reverse = function () {
+	return Array.from(this).reverse().join("");
+}
+
+String.prototype.blank = function() {
+	return (!!this.match(/$\s+^/));
+}
+
+let reverseStr = (string) => {
+	return Array.from(string).reverse().join("");
+}
+
+let palindrome = (string) => {
+	return string.toLowerCase() === reverseStr(string);
+}
+
+let emailParts = (email) => {
+	return email.toLowerCase().split("@");
+}
+
+function Phrase(content) {
+	this.content = content;
+
+	this.palindrome = function palindrome() {
+		return this.processedContent() === this.processedContent().reverse();
+	}
+
+	this.processor = function processor(string) {
+		return string.toLowerCase();
+	}
+
+	this.processedContent = function processedContent() {
+		return this.processor(this.content);
+	}
+
+	this.louder = function louder() {
+		return this.content.toUpperCase();
+	}
+}
+
+function TranslatedPhrase(content, translation) {
+	this.content = content;
+	this.translation = translation;
+
+	this.processedContent = function processedContent() {
+		return this.processor(this.translation)
+	}
+
+}
+
+TranslatedPhrase.prototype = new Phrase();
